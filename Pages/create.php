3189cd260122032d3c06eacc9a0937d3d76c9dad
leogrd49 <leogerard49.pro@gmail.com>
2024-02-06 16:06:41 +0100

    Patch wamp

--- Pages/create.php
+++ Pages/create.php
@@ -1,3 +1,19 @@
 <?php
 include 'navbar.php';
 ?>
+
+<div id="editor">
+  <p>Hello World!</p>
+  <p>Some initial <strong>bold</strong> text</p>
+  <p><br /></p>
+</div>
+
+<!-- Include the Quill library -->
+<script src="https://cdn.jsdelivr.net/npm/quill@2.0.0-rc.0/dist/quill.js"></script>
+
+<!-- Initialize Quill editor -->
+<script>
+  const quill = new Quill("#editor", {
+    theme: "snow",
+  });
+</script>
