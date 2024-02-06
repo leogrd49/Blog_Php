bc400a623ad0ad25de21990b5d432981f608283d
leogrd49 <leogerard49.pro@gmail.com>
2024-02-06 13:50:29 +0100

    TEST page

--- Pages/navbar.php
+++ Pages/navbar.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="shortcut icon" href="img/favicon.png">
     <link href="https://fonts.googleapis.com/css2?family=Poly&display=swap" rel="stylesheet">
     <title>Style Blog</title>
 </head>
