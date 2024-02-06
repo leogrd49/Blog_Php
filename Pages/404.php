634829bfe0cb264172a942afce26628683ca4421
leogrd49 <leogerard49.pro@gmail.com>
2024-02-06 11:06:35 +0100

    Archi

---
+++ Pages/404.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poly&display=swap" rel="stylesheet">
+    <title>404</title>
+</head>
+<body>
+    
+</body>
+</html>
--- Pages/index.php
+++ Pages/index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Style Blog</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+include 'navbar.php';
+?>
