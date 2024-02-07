8d25c6eb524672ac63cb699119cc32903f6f1838
leogrd49 <leogerard49.pro@gmail.com>
2024-02-07 19:03:53 +0100

    patch path

--- Pages/navbar.php
+++ Pages/navbar.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="design.css">
+    <link rel="stylesheet" href="../css/design.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="shortcut icon" href="img/favicon.png">
+    <link rel="shortcut icon" href="../img/favicon.png">
     <link href="https://fonts.googleapis.com/css2?family=Poly&display=swap" rel="stylesheet">
     <title>Style Blog</title>
 </head>
