f3220c4cd39b5bb43db5eade0d27f51598bb01f7
leogrd49 <leogerard49.pro@gmail.com>
2024-02-07 21:13:50 +0100

    WORKINGGGG

--- Pages/create.php
+++ Pages/create.php
@@ -1,3 +1,13 @@
 <?php
 include 'navbar.php';
 ?>
+
+<link href="https://cdn.jsdelivr.net/npm/froala-editor@latest/css/froala_editor.pkgd.min.css" rel="stylesheet" type="text/css" />
+
+<textarea></textarea>
+
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/froala-editor@latest/js/froala_editor.pkgd.min.js"></script>
+
+<script>
+  new FroalaEditor('textarea');
+</script>
