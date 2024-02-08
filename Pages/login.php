1ccf3127885d12be741a564cfd3454647b59f8ee
Maillard <151616522+MaillardLucas@users.noreply.github.com>
2024-02-08 09:54:03 +0100

    update login again

--- Pages/login.php
+++ Pages/login.php
@@ -30,7 +30,7 @@ if (isset($_REQUEST['mail'])and isset($_REQUEST['mdp'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="design.css">
 </head>
 <body>
 
