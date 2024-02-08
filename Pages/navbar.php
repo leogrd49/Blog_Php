b852dc98ddd1aad6fe8d011493de801d633ab8f1
Félis Maillard <151616377+FelisMaillard@users.noreply.github.com>
2024-02-08 09:30:38 +0100

    Partie de félis
    jtm léo

--- Pages/navbar.php
+++ Pages/navbar.php
@@ -8,9 +8,27 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="shortcut icon" href="../img/favicon.png">
     <link href="https://fonts.googleapis.com/css2?family=Poly&display=swap" rel="stylesheet">
-    <title>Style Blog</title>
+    <title>Navbar</title>
 </head>
 <body>
-    
+    <!-- Navbar -->
+
+    <div class="navbar-side">
+        <div class="logo-conatainer-navbar">
+            <img src="img/icons/logo.png" alt="logo de l'entreprise">
+        </div>
+        <div class="icons-container-navbar">
+            <div><a href=""><img src="img/icons/Home.png" alt="icons pour retourner au home"></a></div>
+            <div><a href=""><img src="img/icons/blog.png" alt="icons pour aller à la page de navigation"></a></div>
+            <div><a href=""><img src="img/icons/create.png" alt="icons pour aller à la page de creation"></a></div>
+            <div><a href=""><img src="img/icons/profile.png" alt="icons pour aller à la page profil"></a></div>
+            <div><a href=""><img src="img/icons/setting.png" alt="icons pour aller à la page settings"></a></div>
+        </div>
+        <div class="login-conatainer-navbar">
+            <img src="img/icons/logout.png" alt="icons de logout">
+        </div>
+    </div>
+
+<!-- Fin Navbar -->
 </body>
 </html>
