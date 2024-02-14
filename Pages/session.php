280ec26dd697dd62c87ef4154bb2a59081435e10
Clément.Chevalier <clementchevalier2102@outlook.fr>
2024-02-14 16:55:37 +0100

    session

--- Pages/session.php
+++ Pages/session.php
@@ -4,4 +4,8 @@
         // defini le mode par defaut
         $_SESSION["id"] = 0;
     }
+    if(!isset($_SESSION["log"])) {
+        // defini le mode par defaut
+        $_SESSION["log"] = 0;
+    }
 ?>
