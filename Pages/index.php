68cacd5c1cc9effc78604e51ee4d539e1fb8482c
Félis Maillard <151616377+FelisMaillard@users.noreply.github.com>
2024-02-14 10:09:14 +0100

    Home

--- Pages/index.php
+++ Pages/index.php
@@ -1,3 +1,27 @@
-<?php
-include 'navbar.php';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>StylePost - Home</title>
+    <link rel="stylesheet" href="../css/design.css">
+</head>
+
+
+<body class='home-body'>
+    <div class="left">
+        <div class='home-rose'><img src="../img/rose.png" alt="dessins de rose jolie en png"></div>
+    </div>
+
+    <div class="content">
+        <h1 class='home-h1'>Welcome to StylePost</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia in quo unde tempora placeat, 
+            similique id et illo, dicta consequuntur ut itaque maxime quam. Adipisci fugiat omnis beatae 
+            nemo ipsa?</p>
+        <a class="connect" href="blog.php">
+            <p>Continuer</p>
+        </a>
+    </div>
+
+</body>
+</html>
