a81d7d8ab6163c56462e66d0fe78e00baf535fab
Clément.Chevalier <clementchevalier2102@outlook.fr>
2024-02-14 10:10:39 +0100

    create modif

--- Pages/create.php
+++ Pages/create.php
@@ -1,10 +1,48 @@
 <?php
 include 'navbar.php';
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/froala-editor@latest/css/froala_editor.pkgd.min.css" rel="stylesheet" type="text/css" />
 
-<link href="https://cdn.jsdelivr.net/npm/froala-editor@latest/css/froala_editor.pkgd.min.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+  
+  <form>
+    <label for="titre">Titre de votre blog</label>
+    <input type="text" name='titre'>
+    <label for="auteur">Votre nom</label>
+    <input type="text" name='auteur'>
+    <label for="">Catégorie</label>
+    <select name="categorie">
+      <option value="1">Sport</option>
+      <option value="2">Science</option>
+      <option value="3">Histoire</option>
+      <option value="4">Polotique</option>
+      <option value="5">People</option>
+      <option value="6">Insolite</option>
+    </select>
 
-<textarea></textarea>
+  </form>
+  <textarea name="description"></textarea>
+  <h1>Upload d'image</h1>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        Sélectionnez une image à télécharger :
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <label>Courte description de la photo</label>
+        <input type="text" name="title">
+        <input type="submit" value="Télécharger l'image" name="submit">
+    </form>
+    
+
+</body>
+</html>
+
+
 
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/froala-editor@latest/js/froala_editor.pkgd.min.js"></script>
 
