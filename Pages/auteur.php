d58642f75c1d8d30d8da996c754e0c620cca28b2
Félis Maillard <151616377+FelisMaillard@users.noreply.github.com>
2024-02-15 14:02:11 +0100

    evz

--- Pages/auteur.php
+++ Pages/auteur.php
@@ -29,42 +29,15 @@
             </a>
         </div>
 
-        <div class='carte-blog'>
+        <div class='carte-auteur'>
             
-            <div class="blog-img-container">
+            <div class="auteur-img-container">
                 <a href="#"><img src="../img/blog/img3.jpg" alt=""></a>
-                <a href=""><div class="white-card">
+                <a href=""><div class="white-card-auteur">
                     <h3 class="titre-blog">Titre de l'article</h3>
                     <p class="txt-blog"> Description : dolor sit amet consectetur adipisicing elit. 
                         Cupiditate architecto perferendis, consectetur harum nesciunt quod excepturi ?
                     <p class="txt-blog-2"> Auteur : Lorem ipsum</p></a>
-                </div>
-            </div>
-            <div class='blog-img-container'>
-                <a href="#"><img src="../img/blog/img3.jpg" alt=""></a>
-                <a href=""><div class='white-card'>
-                    <h3 class='titre-blog'>Titre de l'article</h3>
-                    <p class='txt-blog'> Description : dolor sit amet consectetur adipisicing elit. 
-                        Cupiditate architecto perferendis, consectetur harum nesciunt quod excepturi ?
-                    <p class='txt-blog-2'> Auteur : Lorem ipsum</p></a>
-                </div>
-            </div>
-            <div class='blog-img-container'>
-                <a href="#"><img src="../img/blog/img3.jpg" alt=""></a>
-                <a href=""><div class='white-card'>
-                    <h3 class='titre-blog'>Titre de l'article</h3>
-                    <p class='txt-blog'> Description : dolor sit amet consectetur adipisicing elit. 
-                        Cupiditate architecto perferendis, consectetur harum nesciunt quod excepturi ?
-                    <p class='txt-blog-2'> Auteur : Lorem ipsum</p></a>
-                </div>
-            </div>
-            <div class='blog-img-container'>
-                <a href="#"><img src="../img/blog/img3.jpg" alt=""></a>
-                <a href=""><div class='white-card'>
-                    <h3 class='titre-blog'>Titre de l'article</h3>
-                    <p class='txt-blog'> Description : dolor sit amet consectetur adipisicing elit. 
-                        Cupiditate architecto perferendis, consectetur harum nesciunt quod excepturi ?
-                    <p class='txt-blog-2'> Auteur : Lorem ipsum</p></a>
                 </div>
             </div>
 
