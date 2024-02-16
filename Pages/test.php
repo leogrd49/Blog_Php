989965bd345423c6d297ad79e4d3955594c6b73e
Maillard <151616522+MaillardLucas@users.noreply.github.com>
2024-02-16 09:59:22 +0100

    update blog auteur

---
+++ Pages/test.php
@@ -0,0 +1,29 @@
+<?php
+    include 'navbar-connect.php'
+?>
+<!doctype html>
+<html class="no-js" lang="fr"> 
+<head>
+    <link rel="stylesheet" href="../css/design.css">
+</head>
+<body>
+<div class='carte-presentation-auteur-container'>
+            
+            <div class='carte-blog-auteur'>
+                <div class='cyan-part'>
+                    <h3 class='prenom-auteur'>Alexandre</h3>
+                    <h3 class='nom-auteur'>Tsong-li</h3>
+                    <img src="../img/auteurs/asiatpdp.jpg" alt="" class='img-auteur'>
+                    <hr class='barre-auteur'/>
+                </div>
+                <div class='white-part'>
+                    <h3 class='auteur-type'>Alexandre Tsong-Li's blogs :</h3>
+                    <div class="grid-img-blog-auteur">
+                        <img src="..\img\blog\food.jpg" alt="" class='pdp-blog'>
+                        <img src="..\img\blog\it.jpg" alt="" class='pdp-blog'> 
+                        <img src="..\img\blog\it.jpg" alt="" class='pdp-blog'>
+                        <img src="..\img\blog\it.jpg" alt="" class='pdp-blog'>
+                    </div>                
+                </div>
+            </div>
+</body>
